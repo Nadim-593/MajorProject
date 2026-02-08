4e8809a530271c37fb1c4b9588599285b951cd67
Nadim-593 <meo39699@gmail.com>
2026-02-08 22:21:28 +0600

    updated

--- models/reviews.js
+++ models/reviews.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const reviewSchema = new schema ({
+const reviewSchema = new Schema ({
     comment : String,
     rating : {
         type : Number,
@@ -10,7 +10,7 @@ const reviewSchema = new schema ({
     },
     CreateAt : {
         type : Date,
-        default : Date.now();
+        default : Date.now(),
 
     } 
 });
