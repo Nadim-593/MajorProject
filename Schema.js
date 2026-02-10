d6d379bdf65e66e7ee27334bfba9c0f0d214d5b4
Nadim-593 <meo39699@gmail.com>
2026-02-10 17:41:27 +0600

    updated schema

--- Schema.js
+++ Schema.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const reviews = require('./models/reviews');
 module.exports.ListingSchema = Joi.object({
     listing: Joi.object({
         title:Joi.string().required(),
@@ -7,5 +8,12 @@ module.exports.ListingSchema = Joi.object({
         country : Joi.string().required(),
         price : Joi.number().required().min(0),
         image : Joi.string().allow("",null)
+    }).required()
+})
+module.exports.reviewSchema = Joi.object({
+    review : Joi.object({
+        rating : Joi.number().required().min(1).max(5),
+        comment : Joi.string().required(),
+
     }).required()
 })
