6ded12784d9bc7840d5f6e54bd48c71356769b17
Nadim-593 <meo39699@gmail.com>
2026-02-16 11:21:07 +0600

    cookes

--- Classrome/server.js
+++ Classrome/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+
+const cookieParser = require("cookie-parser");
+
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 
@@ -10,8 +13,19 @@ res.send("Hi, I am root!");
 app.use("/users", users);
 app.use("/posts", posts);
 
+app.use(cookieParser());
 
+// Cookies 
+app.get("/cookie", (req,res) => {
+    res.cookie("greed", "value ")
+    res.send("Hi ")
+})
 
+app.get("/cookie/checkName", (req,res)=>{
+    let {name = "Nothing"} = req.cookies;
+    res.send(`Hi ${name}`)
+})
 app.listen(3000, () => {
 console. log("server is listening to 3000");
 });
+
