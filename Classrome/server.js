76601e9da653e7abfea38d308a3dee316c948f44
Nadim-593 <meo39699@gmail.com>
2026-02-17 22:10:00 +0600

    updated

--- Classrome/server.js
+++ Classrome/server.js
@@ -8,8 +8,17 @@ const posts = require("./routes/post.js");
 const cookies = require("./routes/cookies.js");
 const sessionTest = require("./routes/sessionTest.js");
 const cookieParser = require("cookie-parser"); // import cookie-parser
-
 const session = require("express-session")
+const flash = require("connect-flash")
+const path = require("path");
+const ejsMate = require("ejs-mate");
+
+// view engine
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
 const sessionOption = {
     secret : " keyBoard Cat",
     resave: false,
@@ -17,7 +26,7 @@ const sessionOption = {
 }
 
 app.use(session(sessionOption));
-
+app.use(flash());
 
 
 app.use(cookieParser("secretCode"));    //cookie-parser
