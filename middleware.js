7946af1f089a4f4ab5a519532ca15f5155661177
Nadim-593 <meo39699@gmail.com>
2026-03-07 23:52:25 +0600

    updated

--- middleware.js
+++ middleware.js
@@ -1,7 +1,16 @@
  module.exports.IsLoggedIn = (req,res,next) =>{
     if(!req.isAuthenticated()){ // passport e isAuthenticated() invild method
+      //redirect URl 
+      req.session.redirectUrl= req.originalUrl;
    req.flash("error","you Must be Logged in to Create Listing!");
    return res.redirect("/login")
     }
     next();
+ }
+
+ module.exports.saveRedirectUrl= (req,res,next)=>{
+   if(req.session.redirectUrl){
+      res.locals.redirectUrl = req.session.redirectUrl;
+   }
+   next();
  }
