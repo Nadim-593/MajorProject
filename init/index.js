56b02c3bd069a8de8c02773571aa8ca3aac3bc74
Nadim-593 <meo39699@gmail.com>
2026-03-08 02:30:00 +0600

    added owner

--- init/index.js
+++ init/index.js
@@ -19,6 +19,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+ initData.data = initData.data.map((obj)=>({...obj,owner:"69aaed9a05e7b6aafdfad3b8"}));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
